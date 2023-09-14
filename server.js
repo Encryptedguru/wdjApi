24559deedb250167a0ecb05689535bbafe1f9381
Edcryptedguru <edcrypted2@gmail.com>
2023-09-14 07:28:30 +0300

    short circuted port on server

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/dist/WDJ/browser", "index.html"))
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.bgYellow)
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT || 8080}`.bgYellow)
 })
